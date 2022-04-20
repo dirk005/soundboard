ee6d112b0cb1c8cb5c4294f08a435a1d934249b3
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2022-04-20 11:48:47 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const buttons = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+  buttons.appendChild(btn);
+});
+
+const stopSongs = () => {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+};
